59cb7cf73901b0173ec98b32375ffa3ca9990186
YousefNady <yousefnadi01@gmail.com>
2025-10-08 15:25:06 +0200

    Add project files.

---
+++ RouteGymManagementPL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RouteGymManagementPL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
